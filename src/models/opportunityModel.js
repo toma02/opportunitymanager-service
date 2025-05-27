8582323362268f8fd295f0e3bafb2c2a778238b7
toma02 <m.tomasic123.tc@gmail.com>
2025-05-27 14:03:00 +0200

    Add isFull and attendanceCount when fetching events

--- src/models/opportunityModel.js
+++ src/models/opportunityModel.js
@@ -3,95 +3,213 @@ const pool = require('../db');
 const getAll = async (currentUser) => {
   const currentUserSql = currentUser ? `WHERE vo.useridoforganisator != ${currentUser}` : '';
   const sql = `
-  SELECT
-    vo.OpportunityID AS id,
-    vo.OpportunityTitle AS title,
-    vo.Location AS location,
-    TO_CHAR(vo.OpportunityDate, 'HH24:MI') AS time,
-    json_build_object(
-      'day', EXTRACT(DAY FROM vo.OpportunityDate),
-      'month', TO_CHAR(vo.OpportunityDate, 'Mon')
-    ) AS date,
-    vo.duration AS duration,
-    vo.OpportunityDate AS dateTime,
-    vo.Description AS description,
-    json_build_object(
-      'name', u.UserName,
-      'role', 'Organizator volontiranja',
-      'avatar', 'avatar_default' -- Potrebno zamijeniti
-    ) AS organizer,
-    (SELECT FileName FROM EventImages WHERE OpportunityID = vo.OpportunityID LIMIT 1) AS image,
-    (SELECT json_agg(UserID) FROM Attendance WHERE OpportunityID = vo.OpportunityID AND Attended = true LIMIT 5) AS avatars,
-    (SELECT json_agg(json_build_object('id', a.UserID, 'name', u2.UserName))
-    FROM Attendance a
-    JOIN "User" u2 ON a.UserID = u2.UserId
-    WHERE a.OpportunityID = vo.OpportunityID AND a.Attended = true LIMIT 5) AS participants,
-    minimumvolunteers,
-    maximumvolunteers,
-    ridetothedestination,
-    equipmentrequired,
-    latitude,
-    longitude
-    FROM VolunteerOpportunity vo
-    JOIN "User" u ON vo.UserIDOfOrganisator = u.UserId
-    ${currentUserSql}
-    ORDER BY vo.OpportunityDate ASC
-  `;
+            SELECT 
+            vo.OpportunityID AS id, 
+            vo.OpportunityTitle AS title, 
+            vo.Location AS location, 
+            TO_CHAR(vo.OpportunityDate, 'HH24:MI') AS time, 
+            json_build_object(
+              'day', 
+              EXTRACT(
+                DAY 
+                FROM 
+                  vo.OpportunityDate
+              ), 
+              'month', 
+              TO_CHAR(vo.OpportunityDate, 'Mon')
+            ) AS date, 
+            vo.duration AS duration, 
+            vo.OpportunityDate AS dateTime, 
+            vo.Description AS description, 
+            json_build_object(
+              'name', 
+              u.UserName, 
+              'role', 
+              'Organizator volontiranja', 
+              'avatar', 
+              'avatar_default' -- Potrebno zamijeniti
+              ) AS organizer, 
+            (
+              SELECT 
+                FileName 
+              FROM 
+                EventImages 
+              WHERE 
+                OpportunityID = vo.OpportunityID 
+              LIMIT 
+                1
+            ) AS image, 
+            (
+              SELECT 
+                json_agg(UserID) 
+              FROM 
+                Attendance 
+              WHERE 
+                OpportunityID = vo.OpportunityID 
+                AND Attended = true 
+              LIMIT 
+                5
+            ) AS avatars, 
+            (
+              SELECT 
+                json_agg(
+                  json_build_object(
+                    'id', a.UserID, 'name', u2.UserName
+                  )
+                ) 
+              FROM 
+                Attendance a 
+                JOIN "User" u2 ON a.UserID = u2.UserId 
+              WHERE 
+                a.OpportunityID = vo.OpportunityID 
+                AND a.Attended = true 
+              LIMIT 
+                5
+            ) AS participants, 
+            minimumvolunteers, 
+            maximumvolunteers, 
+            (
+              SELECT 
+                COUNT(*) 
+              FROM 
+                Attendance a 
+              WHERE 
+                a.OpportunityID = vo.OpportunityID 
+                AND a.Attended = true
+            ) AS attendeesCount, 
+            (
+              SELECT 
+                COUNT(*) 
+              FROM 
+                Attendance a 
+              WHERE 
+                a.OpportunityID = vo.OpportunityID 
+                AND a.Attended = true
+            ) >= vo.maximumvolunteers AS isFull, 
+            ridetothedestination, 
+            equipmentrequired, 
+            latitude, 
+            longitude 
+          FROM 
+            VolunteerOpportunity vo 
+            JOIN "User" u ON vo.UserIDOfOrganisator = u.UserId ${currentUserSql} 
+          ORDER BY 
+            vo.OpportunityDate ASC
+`;
   const result = await pool.query(sql);
   return result.rows;
 };
 
 const getById = async (eventId, userId) => {
   const sql = `
-  SELECT
-    vo.OpportunityID AS id,
-    vo.OpportunityTitle AS title,
-    vo.Location AS location,
-    TO_CHAR(vo.OpportunityDate, 'HH24:MI') AS time,
-    json_build_object(
-      'day', EXTRACT(DAY FROM vo.OpportunityDate),
-      'month', TO_CHAR(vo.OpportunityDate, 'Mon')
-    ) AS date,
-    vo.duration AS duration,
-    vo.OpportunityDate AS dateTime,
-    vo.Description AS description,
-    json_build_object(
-      'name', u.UserName,
-      'role', 'Organizator volontiranja',
-      'avatar', 'avatar_default'
-    ) AS organizer,
-    (SELECT FileName FROM EventImages WHERE OpportunityID = vo.OpportunityID LIMIT 1) AS image,
-    (SELECT json_agg(UserID) FROM Attendance WHERE OpportunityID = vo.OpportunityID AND Attended = true LIMIT 5) AS avatars,
-    -- Svi prijavljeni korisnici (bez LIMIT-a)
-    (
-      SELECT json_agg(json_build_object('id', a.UserID, 'name', u2.UserName))
-      FROM Attendance a
-      JOIN "User" u2 ON a.UserID = u2.UserId
-      WHERE a.OpportunityID = vo.OpportunityID AND a.Attended = true
-    ) AS participants,
-    (
-      SELECT json_agg(k.Name) 
-      FROM EventKeyword ek
-      JOIN Keyword k ON ek.KeywordID = k.KeywordID
-      WHERE ek.EventID = vo.OpportunityID
-    ) AS keywords,
-    -- Da li je trenutni user prijavljen (vraća true/false)
-    EXISTS(
-      SELECT 1 FROM Attendance att
-      WHERE att.OpportunityID = vo.OpportunityID 
-        AND att.UserID = $2 
-        AND att.Attended = true
-    ) AS "isUserAttending",
-    minimumvolunteers,
-    maximumvolunteers,
-    ridetothedestination,
-    equipmentrequired,
-    latitude,
-    longitude
-  FROM VolunteerOpportunity vo
-  JOIN "User" u ON vo.UserIDOfOrganisator = u.UserId
-  WHERE vo.OpportunityID = $1;
-  `;
+            SELECT 
+            vo.OpportunityID AS id, 
+            vo.OpportunityTitle AS title, 
+            vo.Location AS location, 
+            TO_CHAR(vo.OpportunityDate, 'HH24:MI') AS time, 
+            json_build_object(
+              'day', 
+              EXTRACT(
+                DAY 
+                FROM 
+                  vo.OpportunityDate
+              ), 
+              'month', 
+              TO_CHAR(vo.OpportunityDate, 'Mon')
+            ) AS date, 
+            vo.duration AS duration, 
+            vo.OpportunityDate AS dateTime, 
+            vo.Description AS description, 
+            json_build_object(
+              'name', u.UserName, 'role', 'Organizator volontiranja', 
+              'avatar', 'avatar_default'
+            ) AS organizer, 
+            (
+              SELECT 
+                FileName 
+              FROM 
+                EventImages 
+              WHERE 
+                OpportunityID = vo.OpportunityID 
+              LIMIT 
+                1
+            ) AS image, 
+            (
+              SELECT 
+                json_agg(UserID) 
+              FROM 
+                Attendance 
+              WHERE 
+                OpportunityID = vo.OpportunityID 
+                AND Attended = true 
+              LIMIT 
+                5
+            ) AS avatars, 
+            -- Svi prijavljeni korisnici (bez LIMIT-a)
+            (
+              SELECT 
+                json_agg(
+                  json_build_object(
+                    'id', a.UserID, 'name', u2.UserName
+                  )
+                ) 
+              FROM 
+                Attendance a 
+                JOIN "User" u2 ON a.UserID = u2.UserId 
+              WHERE 
+                a.OpportunityID = vo.OpportunityID 
+                AND a.Attended = true
+            ) AS participants, 
+            (
+              SELECT 
+                json_agg(k.Name) 
+              FROM 
+                EventKeyword ek 
+                JOIN Keyword k ON ek.KeywordID = k.KeywordID 
+              WHERE 
+                ek.EventID = vo.OpportunityID
+            ) AS keywords, 
+            -- Da li je trenutni user prijavljen (vraća true/false)
+            EXISTS(
+              SELECT 
+                1 
+              FROM 
+                Attendance att 
+              WHERE 
+                att.OpportunityID = vo.OpportunityID 
+                AND att.UserID = $2 
+                AND att.Attended = true
+            ) AS "isUserAttending", 
+            minimumvolunteers, 
+            maximumvolunteers, 
+            (
+              SELECT 
+                COUNT(*) 
+              FROM 
+                Attendance a 
+              WHERE 
+                a.OpportunityID = vo.OpportunityID 
+                AND a.Attended = true
+            ) AS attendeesCount, 
+            (
+              SELECT 
+                COUNT(*) 
+              FROM 
+                Attendance a 
+              WHERE 
+                a.OpportunityID = vo.OpportunityID 
+                AND a.Attended = true
+            ) >= vo.maximumvolunteers AS isFull, 
+            ridetothedestination, 
+            equipmentrequired, 
+            latitude, 
+            longitude 
+          FROM 
+            VolunteerOpportunity vo 
+            JOIN "User" u ON vo.UserIDOfOrganisator = u.UserId 
+          WHERE 
+            vo.OpportunityID = $1;
+`;
 
   const result = await pool.query(sql, [eventId, userId]);
   return result.rows[0] || null;
